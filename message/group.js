0f8303240396f1571aec666d8aca34187f1138d4
adengil23 <95447809+adengil23@users.noreply.github.com>
2021-12-09 02:03:36 +0700

    grup.js

--- message/group.js
+++ message/group.js
@@ -8,7 +8,7 @@ const fs = require("fs");
 const { getBuffer, sleep } = require("../lib/myfunc");
 
 let setting = JSON.parse(fs.readFileSync('./config.json'));
-let { botName } = setting
+let { Gilang Store } = setting
 
 module.exports = async(xinz, anj, welcome, left) => {
     const isWelcome = welcome.includes(anj.jid)
@@ -20,14 +20,14 @@ module.exports = async(xinz, anj, welcome, left) => {
         if (anj.participants[0] === xinz.user.jid){
             await sleep(5000)
             xinz.updatePresence(anj.jid, Presence.composing)
-            xinz.sendMessage(anj.jid, `Hai aku ${botName}, silahkan kirim #menu`, MessageType.text)
+            xinz.sendMessage(anj.jid, `Hai Saya ${Gilang Store}, silahkan kirim #menu`, MessageType.text)
         } else if (isWelcome){
             try {
                 var pic = await xinz.getProfilePicture(anj.participants[0])
             } catch {
                 var pic = 'https://i.ibb.co/Tq7d7TZ/age-hananta-495-photo.png'
             }
-            xinz.sendMessage(anj.jid, await getBuffer(pic), MessageType.image, {caption: `Hai @${anj.participants[0].split("@")[0]}, selamat datang di ${groupName}`, contextInfo: {"mentionedJid": [anj.participants[0]]}})
+            xinz.sendMessage(anj.jid, await getBuffer(pic), MessageType.image, {caption: `Hai @${anj.participants[0].split("@")[0]}, selamat datang di ${CLIENT SUNDACITYHOST}`, contextInfo: {"mentionedJid": [anj.participants[0]]}})
         }
     } else if (anj.action === 'remove' && isLeft){
         try {
